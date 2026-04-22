221acde13f586befad8fc546d488199491957f81
Yasser <yasser.nadi2004@gmail.com>
2026-04-22 16:27:16 +0100

    seting up environements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="yasser N",
+    author_email="yasser.nadi2004@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
